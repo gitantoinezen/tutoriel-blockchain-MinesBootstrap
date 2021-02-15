6ba058b265604ca047cb8830bf5315d9db7ed6c8
Antoine ZEN <zen.antoine@gmail.com>
2021-02-15 16:31:36 +0100

    Block mining ok

--- block.py
+++ block.py
@@ -5,4 +5,52 @@ from key import verify_signature
 
 
 class Block:
-    pass
+    def __init__(self, blockNumber:int, lastHash:str, timestamp=0) -> None:
+        self.transactionList : list[Transaction] = []
+        self.blockNumber = blockNumber
+        self.minerName = ""
+        self.timestamp = timestamp
+        self.last_hash = lastHash
+        self.hashval = ""
+        self.nonce = 0
+
+    def __repr__(self) -> str:
+        string = "Block id: " + str(self.blockNumber) + "\n" + \
+            "prev hash: " + self.last_hash + "\n" + \
+            "hash: " + self.hashval + "\n" + \
+            "Miner: " + self.minerName + "\n" +\
+            "Nonce: " + str(self.nonce) + "\n"
+        return string
+
+    def add_transaction(self,transaction:Transaction) -> None:
+        self.transactionList.append(transaction)
+
+    def hash_func(self):
+
+        sha = hashlib.sha256()
+        data = ""
+        data  = str(self.blockNumber) + str(self.nonce) + str(self.last_hash)
+        for elem in self.transactionList:
+            data+= str(elem.sender) + str(elem.receiver) + str(elem.amount)
+        sha.update(data.encode())
+
+        return sha.hexdigest()
+
+    def check_hash(self, hash:str, difficulty = 0):
+        return hash.startswith("0" * difficulty)
+
+    def verify(self):
+        return self.hashval == self.hash_func()       
+    
+
+    def mine(self, difficulty:int):
+        
+        self.timestamp = time.time()
+
+        while not self.check_hash(self.hash_func(),difficulty):
+            self.nonce += 1
+        
+        self.minerName = "Antoine"
+        self.hashval = self.hash_func()   
+        
+
--- transaction.py
+++ transaction.py
@@ -1,5 +1,30 @@
-from key import verify_signature
+from key import verify_signature,BitcoinAccount
 
 
 class Transaction:
-    pass
+    def __init__(self,sender:str,receiver:str,quantity:float = 0.0,time = 0, tx_number=None) -> None:
+        self.sender = sender
+        self.receiver = receiver
+        self.amount = quantity
+        self.timestamp = time
+        self.tx_number = tx_number
+
+    def __repr__(self):
+
+        string = "Transaction number: " + str(self.tx_number) + "\n" + \
+                "Sender: " + str(self.sender) + "\n" + \
+                "Receiver: " + str(self.receiver) + "\n" + \
+                "Amount: " + str(self.amount) + "\n" + \
+                "Timestamp: " + str(self.timestamp) + "\n"
+
+        return string
+
+    def to_dict(self):
+        tx_dict = {}
+        tx_dict["tx_number"] = self.tx_number
+        tx_dict["sender"] = self.sender
+        tx_dict["receiver"] = self.receiver
+        tx_dict["amount"] = self.amount
+        tx_dict["timestamp"] = self.timestamp
+
+        return tx_dict
